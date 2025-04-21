84b5b88cea858f2328fa28b84b628f0953384fa7
MaxTee33 <144581261+MaxTee33@users.noreply.github.com>
2025-04-21 17:59:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,15 @@ import pandas as pd
 st.title('🤯 Wastewater Treatment Plants')
 st.info('Clustering Energy Consumption Profiles')
 
-df = pd.read_csv('https://raw.githubusercontent.com/MaxTee33/dp-ML/refs/heads/master/Data-Melbourne_F_fixed.csv')
-df
+with st.expender("Data"):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/MaxTee33/dp-ML/refs/heads/master/Data-Melbourne_F_fixed.csv')
+  df
+
+  st.write('**X**')
+  X = df.drop('Average Outflow', axis=1)
+  X
+
+  st.write('***Y*')
+  Y = df.species
+  Y
