2ffd3e1b5db3751bfad69e04cfb93e66ef953a22
MaxTee33 <144581261+MaxTee33@users.noreply.github.com>
2025-04-22 00:37:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,8 +68,8 @@ st.write("Input Data", input_df)
 
 with st.expander('HDBSCAN'):
   scaler = StandardScaler()
-  df_scaled = scaler.fit_transform(input_df)
-  model = hdbscan.HDBSCAN(min_cluster_size=1) # You can adjust min_cluster_size
+  df_scaled = scaler.fit_transform([input_df])
+  model = hdbscan.HDBSCAN(min_cluster_size=3) # You can adjust min_cluster_size
   model.fit(df_scaled)
   cluster_labels = model.labels_
   cluster_output = pd.DataFrame({'Cluster': cluster_labels})
