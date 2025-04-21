71996fb69d97421bbe9e84db7ef5a6bd7dd4b724
MaxTee33 <144581261+MaxTee33@users.noreply.github.com>
2025-04-21 17:25:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ st.title('🤯 Wastewater Treatment Plants')
 st.info('Clustering Energy Consumption Profiles')
 
 df = pd.read_csv('https://raw.githubusercontent.com/MaxTee33/dp-ML/refs/heads/master/Data-Melbourne_F_fixed.csv')
+df
