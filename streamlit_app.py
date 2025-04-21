eb13836f0fd557a1a5d8edc9c453726c1c6b9ec1
MaxTee33 <144581261+MaxTee33@users.noreply.github.com>
2025-04-21 17:25:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import missingno as msno
-from sklearn.preprocessing import StandardScaler
-import warnings
 
 st.title('🤯 Wastewater Treatment Plants')
 st.info('Clustering Energy Consumption Profiles')
