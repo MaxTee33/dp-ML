e363dfab2753bfeef72dede475e0ddb90c16e2bf
MaxTee33 <144581261+MaxTee33@users.noreply.github.com>
2025-04-23 20:20:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 
-st.title('🤯 Wastewater Treatment Plants')
-st.info('Clustering Energy Consumption Profiles')
+
 
 
 df = pd.read_csv('https://raw.githubusercontent.com/MaxTee33/dp-ML/refs/heads/master/processed_data.xls')
