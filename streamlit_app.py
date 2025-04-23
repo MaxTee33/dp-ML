cdd3325a9e2da0986f27505781e0ea5f323e7695
MaxTee33 <144581261+MaxTee33@users.noreply.github.com>
2025-04-23 17:00:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,7 @@ with st.expander('Clusters'):
   X = df[valid_selection]
   num_entries = X.shape[0]
 
-  num_rows = st.slider("Select a range of values", 0, 1100，100)
+  num_rows = st.slider("Select a range of values", 0, 1100)
   st.write("Values:", num_rows)
   
   scaler = StandardScaler()
