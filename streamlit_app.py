1042574993577dd6fa606e3537e1916ede845b6a
MaxTee33 <144581261+MaxTee33@users.noreply.github.com>
2025-04-24 08:24:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,7 +196,7 @@ with st.expander('Mean Shift'):
         # StandardScaler and PCA
         scaler = StandardScaler()
         X_scaled = scaler.fit_transform(df_selected[:num_rows])  # Scale only the selected row
-        pca = PCA(n_components=3)
+        pca = PCA(n_components=2)
         X_pca = pca.fit_transform(X_scaled)
         
         # Apply Mean Shift
